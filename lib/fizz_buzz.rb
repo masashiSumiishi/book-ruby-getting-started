0f3d799ec0eac7bbe325ef28a89fd4b6c9b361ef
Masashi <t15519ms@gmail.com>
2021-07-24 13:39:13 +0900

    add FizzBuzzメソッドを追加

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,1 +1,17 @@
-puts 'hello,world'
+def fizz_buzz(n)
+  if n % 15 == 0
+    p 'FizzBuzz'
+  elsif n % 3 == 0
+    p 'Fizz'
+  elsif n % 5 == 0
+    p 'Buzz'
+  else
+    p n.to_s
+  end
+end
+fizz_buzz(1)
+fizz_buzz(3)
+fizz_buzz(4)
+fizz_buzz(5)
+fizz_buzz(6)
+fizz_buzz(15)
