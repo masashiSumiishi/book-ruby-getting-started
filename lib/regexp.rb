4ff75b914182b18da800c9edae09bb2828f741c2
Masashi <t15519ms@gmail.com>
2021-07-27 22:09:48 +0900

    add 正規表現に関するメソッドをregexp.rbに新規で追加

--- lib/regexp.rb
+++ lib/regexp.rb
@@ -19,6 +19,14 @@ p m[1]
 p m[2]
 p m[3]
 
+text3 =~ /(\d+)年(\d+)月(\d+)日/
+p $~
+p $&
+p $1
+p $2
+p $3
+p $+
+
 n = /(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/.match(text3)
 p n[:year]
 p n[:month]
@@ -27,3 +35,14 @@ p n[:day]
 text4 = "123,456-789"
 hash = {','=>':', '-'=>'/'}
 p text4.gsub(/,|-/, hash)
+
+text5 = '03-1234-5678'
+
+case text5
+when /^\d{3}-d{4}$/
+  puts '郵便番号'
+when /^\d{4}\/\d{1,2}\/\d{1,2}$/
+  puts '日付'
+when /^\d+-\d+-\d+$/
+  puts '電話番号'
+end
